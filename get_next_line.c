5bce2af747d95003f7df9a0de9fbd4c28e2afa35
Mariame Vissargova <mariamevissargova@Ordinateur-portable-de-Mariame.local>
2024-11-22 13:21:25 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,50 +1,59 @@
-int	ft_strlen(char *str)
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 302
+# endif
+
+char	*ft_strdup(char *src)
 {
-	int	i;
+	int i;
+	char *dest;
 
 	i = 0;
-	while (str[i])
+	while (src[i])
 		i++;
-	return (i);
-}
-
-char	*ft_strdup(char *str)
-{
-	int		len;
-	int		i;
-	char	*dup;
-
-	len = ft_strlen(str);
-	i = -1;
-	dup = malloc(len + 1);
-	if (!dup)
-		return (NULL);
-	while (str[++i])
-		dup[i] = str[i];
-	dup[i] = 0;
-	return (dup);
+	dest = malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while(src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 char	*get_next_line(int fd)
 {
-	char	buffer;
-	char	rtn[7000000];
-	int		n;
-	int		i;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
+	int	i;
+	char	line[70000];
+	static int buffer_pos;
+	static int buffer_read;
+	static char buffer[BUFFER_SIZE];
+		
 	i = 0;
-	n = read(fd, &buffer, 1);
-	while (n > 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
 	{
-		rtn[i++] = buffer;
-		if (buffer == '\n')
-			break ;
-		n = read(fd, &buffer, 1);
+		if (buffer_pos >= buffer_read)
+		{
+			buffer_read = read(fd, buffer, BUFFER_SIZE);
+			buffer_pos = 0;
+			if (buffer_read <= 0)
+				break;
+		}
+		if (line[i] == '\n')
+			break;
+		line[i] = buffer[buffer_pos];
+		i++;
+		buffer_pos++;
 	}
-	rtn[i] = 0;
-	if (n <= 0 && i == 0)
-		return (0);
-	return (ft_strdup(rtn));
+	line[i] = '\0';
+	if (i == 0)
+		return (NULL);
+	return (ft_strdup(line));
 }
